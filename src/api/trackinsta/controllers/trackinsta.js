1402f8734a7e67f27206c52e63bfb72d210c2c63
Meherab <meherab0608@gmail.com>
2023-11-28 16:16:02 +0600

    initial commit

---
+++ src/api/trackinsta/controllers/trackinsta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trackinsta controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::trackinsta.trackinsta');
---
+++ src/api/trackinsta/routes/trackinsta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * trackinsta router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::trackinsta.trackinsta');
